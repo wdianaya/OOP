81a034c8189bd66e591f7439aa5352e06f3c8e63
diana <diana.yarmykh14@list.ru>
2026-09-08 11:49:38 +0700

    add shell script

--- Task_1_1_1/run.sh
+++ Task_1_1_1/run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 
+javac -d bin src/main/java/org/example/HeapSort.java
+javadoc -d docs src/main/java/org/example/HeapSort.java
+jar cvfe heapsort.jar org.example.HeapSort -C bin .
+java -jar heapsort.jar
