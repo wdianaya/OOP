8c465de0a7996d08380f733f0b583f816675d800
diana <diana.yarmykh14@list.ru>
2026-09-08 19:45:53 +0700

    changes added for javadoc to the shell script

--- Task_1_1_1/run.sh
+++ Task_1_1_1/run.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-javac -d bin src/main/java/org/example/HeapSort.java
-javadoc -d docs src/main/java/org/example/HeapSort.java
-jar cvfe heapsort.jar org.example.HeapSort -C bin .
+javac -d build/classes src/main/java/org/example/HeapSort.java
+javadoc -encoding UTF-8 -docencoding UTF-8 -charset UTF-8 -d docs src/main/java/org/example/HeapSort.java
+jar cfe heapsort.jar org.example.HeapSort -C build/classes .
 java -jar heapsort.jar
